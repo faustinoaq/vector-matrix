39f5df8356c129450860af2b43ed839235a68a51
Faustino Aguilar <faustinoaq@gmail.com>
2017-09-25 22:19:42 -0500

    Remove file

--- playground/vector.cr
+++
@@ -1,1 +0,0 @@
-["Proceso52", {quantum: 10}, {instrucciónes: 47}, "Proceso93", {quantum: 10}, {instrucciónes: 75}, "Proceso81", {quantum: 10}, {instrucciónes: 93}, "Proceso89", {quantum: 10}, {instrucciónes: 55}, "Proceso34", {quantum: 10}, {instrucciónes: 27}, "Proceso20", {quantum: 10}, {instrucciónes: 37}]
