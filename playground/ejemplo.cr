32e096eb7fb88c5725a1c618cbdd326786a34f65
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2017-12-04 12:05:05 -0500

    Add logging

--- playground/ejemplo.cr
+++ playground/ejemplo.cr
@@ -4,9 +4,10 @@ M1 = Matriz[
   1, 2, 3, 5,
   4, 7, 1, 2,
   columnas: 4,
-  nombre: "M1"]
+  nombre: "M1",
+  log: true]
 
-A = Matriz[1, 2, 3, 4, 2, 1, 6, 1, columnas: 2, nombre: "A"]
+A = Matriz[1, 2, 3, 4, 2, 1, 6, 1, columnas: 2, nombre: "A", log: true]
 
 puts "Resultado usando vectores:"
 
