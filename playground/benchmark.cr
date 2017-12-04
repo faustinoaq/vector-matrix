1dcaf933d22900850ebcce8f943ff1f85135e720
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2017-12-04 12:02:14 -0500

    Update package

--- playground/benchmark.cr
+++ playground/benchmark.cr
@@ -1,13 +1,14 @@
 require "benchmark"
-require "./playground/matriz"
+require "./matriz"
 
 M1 = Matriz[
   1, 2, 3, 5,
   4, 7, 1, 2,
   columnas: 4,
-  nombre: "M1"]
+  nombre: "M1",
+  log: false]
 
-A = Matriz[1, 2, 3, 4, 2, 1, 6, 1, columnas: 2, nombre: "A"]
+A = Matriz[1, 2, 3, 4, 2, 1, 6, 1, columnas: 2, nombre: "A", log: false]
 
 require "linalg"
 
